ec317ce394c99efd82b148cc042ad9748dd2db65
houbin <1031248990@qq.com>
2016-11-26 15:06:47 +0800

    es module for fb

--- fb_start.sh
+++ fb_start.sh
@@ -4,5 +4,11 @@
 #sed 
 
 echo "filebeat 5.0.0 starting ..."
+var=${ES_IP_PORT} 
+var=${var//,/ } 
+for one_of_es_add in $var   
+do  
+    curl -XPUT 'http://'${one_of_es_add}'/_template/filebeat' -d@/etc/filebeat/filebeat.template.json
+done  
 /usr/share/filebeat/bin/filebeat -e -c "/etc/filebeat/filebeat.yml"
 echo "filebeat 5.0.0 started ..."
